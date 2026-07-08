ebbcd6c76c83552842eac86a83875a25522cce9c
Youngbin Ko <66235935+youngbin0417@users.noreply.github.com>
2026-07-08 14:31:06 +0900

    Refactor/data policy update (#2)
    * docs: yaml의 amazon 물젖음이 운영 정책서와 충돌하여 amazon chunk 삭제, 운영정책서에 '부분환불' 추가하고, 해외도서에 관한 규정 추가
    
    * chore: RAG전용 postgreSQL db(pgvector 사용) 연결 세팅
    
    * chore: RAG전용 chromadb  연결로 기본 세팅
    
    * docs: WMS 표준 운영 정책서 내 DISCOUNT_STOCK 감가율 지침 업데이트
    
    * docs: 칸반, 실무 업무가이드 수정
    
    ---------
    
    Co-authored-by: starskein <world2013@naver.com>

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "B2B WMS Platform API"
     DATABASE_URL: str = "postgresql://admin:password@localhost:5432/wms_db"
+    CHROMA_SERVER_HOST: str = "localhost"
+    CHROMA_SERVER_PORT: int = 8001
     # AWS, Supabase or other config can be added here
     
     class Config:
